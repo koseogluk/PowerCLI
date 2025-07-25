e7c74910db03430002a108789b6953ac61f476e3
koseogluk <koseoglu.kamer@gmail.com>
2025-07-25 13:56:52 +0300

    Snapshot Add - Delete Script

---
+++ snapshot-all.ps1
@@ -0,0 +1,43 @@
+﻿# vCenter'a bağlan
+connect-viserver -server "vCenter Hostname" -credential (get-credential)
+
+# Snapshot alma işlemi
+$hostnameFile = "C:\snap-list.txt"
+
+$snapshotName = "Linux Security Patch 2024"
+$snapshotDescription = "Emrah Uludag"
+$hostnames = Get-Content -Path $hostnameFile
+
+foreach ($hostname in $hostnames) {
+    $vm = Get-VM -Name $hostname -ErrorAction SilentlyContinue
+    if ($vm -ne $null) {
+        Write-Output "Taking snapshot for VM: $hostname"
+        New-Snapshot -VM $vm -Name $snapshotName -Description $snapshotDescription -Memory:$false -Quiesce:$true
+        Write-Output "Snapshot taken for VM: $hostname"
+    } else {
+        Write-Output "VM not found: $hostname"
+    }
+}
+
+# Snapshot silme işlemi
+$hostnameFile = "C:\snap-listtxt"
+$hostnames = Get-Content -Path $hostnameFile
+
+
+foreach ($hostname in $hostnames) {
+    
+    $vm = Get-VM -Name $hostname -ErrorAction SilentlyContinue
+    if ($vm -ne $null) {
+        Write-Output "Deleting snapshots for VM: $hostname"
+        $snapshots = Get-Snapshot -VM $vm
+        foreach ($snapshot in $snapshots) {
+            Remove-Snapshot -Snapshot $snapshot -Confirm:$false
+            Write-Output "Snapshot deleted for VM: $hostname - Snapshot: $($snapshot.Name)"
+        }
+    } else {
+        Write-Output "VM not found: $hostname"
+    }
+}
+
+# vCenter bağlantısını kapat
+Disconnect-VIServer -Confirm:$false
